990c59b3e0c6c2ecb06f9911ae2094263a9532fd
serotonine <ks.serotonine@gmail.com>
2024-02-22 17:08:39 +0100

    Reusing external styled-components.

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { styled } from "styled-components";
+import CustomInput from "./styled-components/StyledInput";
 
 /*
  * Will created a div as a STYLED component
@@ -10,30 +11,6 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-/*
- * You can still add properties and className in this styled component.
- * and ALSO set conditionals at component setting level.
- * By convention the styling props are prefixed with an $
- */
-const ControlLabel = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-const ControlInput = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
 /*
@@ -78,22 +55,20 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <ControlLabel $invalid={emailNotValid}>Email</ControlLabel>
-          <ControlInput
+          <CustomInput
+            label="Email"
             type="email"
-            $invalid={emailNotValid}
-            onChange={(event) =>
-              handleControlInputChange("email", event.target.value)
-            }
+            invalid={emailNotValid}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <ControlLabel $invalid={passwordNotValid}>Password</ControlLabel>
-          <ControlInput
+          <CustomInput
+            label="Password"
             type="password"
-            $invalid={passwordNotValid}
+            invalid={emailNotValid}
             onChange={(event) =>
-              handleControlInputChange("password", event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>

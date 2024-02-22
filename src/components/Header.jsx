e9ab01e4c76122aecd65c17afc8f54971f470bc3
serotonine <ks.serotonine@gmail.com>
2024-02-22 14:35:11 +0100

    Begin styled-components.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import style from "../css/Header.module.css";
 
 export default function Header() {
+  const machin = false;
   return (
-    <header>
+    <header className={style.header}>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
+
+      <p
+        /* Also the css property could be dynamic. */
+        style={{
+          color: machin ? "red" : "blue",
+          textTransform: "uppercase",
+        }}
+      >
+        This a style prop example
+      </p>
     </header>
   );
 }
